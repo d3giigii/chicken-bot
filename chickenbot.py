12fe9e9a7e03e3341053412c26d552ffcc254bb9
d3giigii <lhammond997@gmail.com>
2021-12-18 14:09:44 -0500

    update

--- chickenbot.py
+++ chickenbot.py
@@ -3,11 +3,14 @@
 
 """Bot for SNM chicken cult server. Made by d3giigii."""
 
+# import asyncio
 import configparser
 import discord
 import logging
+import youtube_dl
 
 from discord import client
+from discord.errors import ClientException
 from discord.ext import commands
 
 # Parse the config file. 
@@ -19,6 +22,47 @@ config.read('token.ini')
 # Setup from config
 prefix_char = str(config.get('OPTIONS', 'PrefixChar')) 
 bot = commands.Bot(command_prefix=prefix_char)
+ffmpeg_path = "C:\\Users\\sonof\\ffmpeg\\bin\\ffmpeg.exe"
+err_str = "Error: "
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, 
+                                          download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), 
+            data=data)
 
 # Command handling
 @bot.event 
@@ -31,6 +75,26 @@ async def on_message(message):
 @bot.command()
 async def cluck(ctx):
     await ctx.send("cluck")
+
+@bot.command()
+async def play(ctx, input : str):
+    await disconnect(ctx)
+    try:
+        player = await YTDLSource.from_url(input, stream=True)
+        channel = ctx.author.voice.channel
+        vc = await channel.connect()
+        vc.play(player)
+        await ctx.send("Playing " +player.title)
+    except (AttributeError, ClientException, RuntimeError) as e:
+        await ctx.send(err_str, e.__str__())
+
+@bot.command()
+async def disconnect(ctx):
+    server = ctx.message.guild.voice_client
+    try:
+        await server.disconnect()
+    except AttributeError:
+        pass
 
 # On driver start. 
 @bot.event 
